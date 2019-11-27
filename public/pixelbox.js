1ac4485b206f5e8ccfe04346037eff96657201d1
Luke Peña <luketpena@gmail.com>
2019-11-27 12:33:43 -0600

    Calculate layer size

--- public/pixelbox.js
+++ public/pixelbox.js
@@ -4,14 +4,14 @@ let mouseMov = [0,0]; //movement from -1 to 1
 let frameList = [];
 
 $(document).ready(()=>{
-  console.log('JQ');
   pbRun();
 })
 
 function pbCreate (target,bkg,images,str,size,overlap,display) {
   //Find the number of layers based on the arguments provided
   const layerCount = Math.min(images.length,str.length);
-  console.log('Number of layers:',layerCount);
+  //Find the ratio of the img to the frame size
+  const ratio = [display[0]/size[0],display[0]/size[0]];
 
   //Add the background to the frame
   $(target).css('background',`url(${bkg})`);
@@ -19,21 +19,27 @@ function pbCreate (target,bkg,images,str,size,overlap,display) {
   $(target).css('width',display[0]);
   $(target).css('height',display[1]);
 
+  let layerSize = [(size[0]+(overlap[0]*2))*ratio[0],(size[1]+(overlap[1]*2))*ratio[1]];
+
   //Create the layer jQuery objects
   const layers = [];
   for (let i=0; i<layerCount; i++) {
     //Create and style the layers
     const newLayer = $(`<div class='pb-layer'></div>`);
     newLayer.css('background',`url(${images[i]})`);
-    newLayer.css('background-size',`${display[0]}px ${display[1]}px`);
+    newLayer.css('background-size',`${layerSize[0]}px ${layerSize[1]}px`);
+    newLayer.css('background-position','center');
+    newLayer.css('width',layerSize[0]);
+    newLayer.css('height',layerSize[1]);
+    newLayer.css('left',-(overlap[0]*ratio[0]));
+    newLayer.css('top',-(overlap[1]*ratio[1]))
+    console.log('Full width:',display[0]+(overlap[0]*ratio[0]));
     //Add the layers to the array and the frame
     layers.push(newLayer);
     $(target).append(newLayer);
   }
 
-  //Find the ratio of the img to the frame size
-  const ratio = [display[0]/size[0],display[0]/size[0]];
-  console.log('Ratio:',ratio);
+
 
   //Create the attributes object
   const attr = {target,bkg,images,str,size,display,overlap,layers,layerCount,ratio};
